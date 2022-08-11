c350758ab06b7559115b1d86fd216127bf0c9830
afeiship <1290657123@qq.com>
2022-08-11 16:52:48 +0800

    wip

--- bin/index.js
+++ bin/index.js
@@ -29,7 +29,7 @@ const program = new Command();
 const SECRETS = {
   'idea': ['https://www.ajihuo.com/idea/4222.html', 880805],
   'pycharm': ['https://www.ajihuo.com/pycharm/4197.html', 550809],
-  'webstorm': ['https://www.ajihuo.com/webstorm/4188.html', 180729],
+  'webstorm': ['https://www.ajihuo.com/webstorm/4188.html', 180809],
   '52shizhan:goland': ['goland', 4300],
   '52shizhan:pycharm': ['pycharm', 4421],
   '52shizhan:webstorm': ['webstorm', 2588],
